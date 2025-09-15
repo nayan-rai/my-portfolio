2331479183ccdd54f80ed784be9f57ed286e8939
Vishal <vishal@mychanakya.com>
2025-09-15 19:11:13 +0530

    Initial commit of the portfolio website

---
+++ MyPortfolio-Nayan-main/src/data/constants.js
@@ -0,0 +1,553 @@
+export const Bio = {
+  name: "Nayan Rai",
+  roles: [
+    "Backend Developer",
+    "Devops Engineer",
+  ],
+  description:
+    "I'm an Associate Staff Engineer with a passion for designing and implementing robust, scalable backend systems. I excel at architecting clean, efficient solutions and have a proven track record in building distributed systems for diverse domains, including TeleCom, Aviation and Shipping & Logistics.",
+  github: "https://github.com/nayan-rai",
+  resume: "https://docs.google.com/document/d/1c9_i1nfdQ7efR0R9Pn_1bQ-cJx9SdupT3f4fbQz15PY/edit?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/nayanrai/",
+  twitter: "https://twitter.com/Himmu_S03",
+/*  insta: "https://www.instagram.com/",
+  topmate: "https://topmate.io/",
+  medium: "https://medium.com/"*/
+};
+
+export const skills = [
+
+
+
+  {
+      title: "Programming Languages",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "C/C++",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/3/32/C%2B%2B_logo.png",
+        },
+        {
+          name: "Python",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+        },
+      ],
+    },
+
+    {
+    title: "Frameworks",
+    skills: [
+    {
+            name: "Spring Framework",
+            image: "https://media.licdn.com/dms/image/D4D12AQEICFOK-z0akA/article-cover_image-shrink_720_1280/0/1656604745399?e=2147483647&v=beta&t=-Fjm5QYiJOJNzJ89xGv9VYPNxMNxqIX4jscMWXB6VaM",
+    },
+    {
+      name: "Hibernate",
+      image:
+        "https://icon.icepanel.io/Technology/svg/Hibernate.svg",
+    },
+      {
+            name: "Apache Camel",
+            image:
+              "https://i.pinimg.com/originals/00/dc/03/00dc0377822a2698d1575e3cec6a9258.png",
+      },
+ {
+              name: "Spark",
+              image:
+                "https://upload.wikimedia.org/wikipedia/commons/f/f3/Apache_Spark_logo.svg",
+            },
+    ],
+  },
+
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Angular",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png",
+      },
+    ],
+  },
+{
+    title: "Backend",
+    skills: [
+    {
+                  name: "Apache Kafka",
+                  image:
+                    "https://pbs.twimg.com/profile_images/781633389577195521/kazUJooF_400x400.jpg",
+          },
+      {
+        name: "Microservices",
+        image:
+          "https://t4.ftcdn.net/jpg/03/85/29/69/360_F_385296927_b9V4skbl94TzxL8AiVAMGdpp9hFeZat0.jpg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+                    name: "MariaDB",
+                    image:
+                      "https://i0.wp.com/daniel-bartholomew.com/wp-content/uploads/2020/05/MariaDB-Seal-with-Text-alt.png?resize=589%2C479&ssl=1",
+                  },
+    ],
+  }
+  ,
+  
+  {
+    title: "Devops",
+    skills: [
+      {
+        name: "Docker",
+        image:
+          "https://icon.icepanel.io/Technology/svg/Docker.svg",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Kubernetes_logo_without_workmark.svg/1200px-Kubernetes_logo_without_workmark.svg.png",
+      },
+      {
+        name: "Jfrog",
+        image: "https://d15shllkswkct0.cloudfront.net/wp-content/blogs.dir/1/files/2022/05/jfrog.png",
+      },
+      {
+        name: "AWS Cloud",
+        image:
+          "https://www.pngall.com/wp-content/uploads/13/AWS-Logo-PNG-File.png",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://logos-world.net/wp-content/uploads/2020/09/Linux-Logo.png",
+      },
+      {
+        name: "Jenkins CI/CD",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Jenkins_logo.svg/1200px-Jenkins_logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Other Tools",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEkW5QVyzoVT_A8dA6aG4Y6mf_q64EjDpnpRxm49mvUg&s",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png",
+      },
+   
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png",
+      },
+      {
+        name: "Intellij IDEA",
+        image:
+          "https://www.vhv.rs/dpng/d/527-5279597_intellij-idea-logo-png-transparent-png.png",
+      },
+      {
+        name: "Sublime Text",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/7/7a/Sublime_text.png?20241023104807",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+
+  {
+    id: 0,
+    img: "https://d3bql97l1ytoxn.cloudfront.net/app_resources/221304/thumbs_112/img28765405970016105-2x.png",
+    role: "Associate Staff Engineer (Java Backend Developer)",
+    company: "Nagarro",
+    date: "Dec 2024 - Present",
+    desc: "Working with Lufthansa group for Revenue Management products",
+    skills: [
+      "Java",
+      "Python",
+      "Spring boot",
+      "Microservice",
+      "Docker",
+      "Apache Kafka",
+      "CI/CD Jenkins",
+      "Junit",
+      "JFrog",
+      "AWS",
+      "Dbeaver",
+      "Spark",
+      "R"
+    ],
+  },
+
+    {
+    id: 1,
+    img: "https://d3bql97l1ytoxn.cloudfront.net/app_resources/221304/thumbs_112/img28765405970016105-2x.png",
+    role: "Senior Engineer (Java Backend Developer)",
+    company: "Nagarro",
+    date: "May 2023 - Nov 2024",
+    desc: "Worked on the Shipping & Logistics products for Flipkart",
+    skills: [
+      "Java",
+      "Spring boot",
+      "Microservice",
+      "Docker",
+      "Apache Camel",
+      "Apache Kafka",
+      "CI/CD Jenkins",
+      "Junit",
+      "JFrog",
+      "AWS",
+      "Dbeaver"
+    ],
+  },
+  {
+    id: 2,
+    img: "https://assets.telecomtv.com/assets/telecomtv/rakuten-symphony-logo-12947.jpeg?w=1280&h=720&crop=auto",
+    role: "Software Engineer (Java BackEnd Developer)",
+    company: "Rakuten Symphony",
+    date: "Jan 2020 - Apr 2023",
+    desc: "Worked on internal OSS and BSS products",
+    skills: [
+      "Java",
+      "Spring boot",
+      "Microservice",
+      "Docker",
+      "AWS",
+      "Apache Camel",
+      "Apache Kafka",
+      "CI/CD Jenkins",
+      "Junit",
+
+    ],
+  
+  },
+  
+];
+
+export const education = [
+
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C560BAQGVF2qj__3qtg/company-logo_400_400/0/1646318126580?e=2147483647&v=beta&t=m_pWngulU4pmV7TEjPVS2V6BCiKactu8PRf0FGQoWwk",
+    school: "Chameli Devi Group Of Institutions, Indore",
+    date: "May 2016 - May 2020",
+    desc: "I have done my graducation in Bachelor's degree in Computer Science Engineering at Chameli Devi Group Of Institutions, Indore. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. ",
+    degree: "Bachelor of Technology - BTech, Computer Science Engineering",
+  },
+
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6KtCz4kQ5SEhlbeDe2DO8AuOxdyrW1UmrZKzsnwEfZcmcV0QNJsX2I82DZcfC_Oq_cow&usqp=CAU",
+    school: "Adarsh Bal Vinay Mandir, Indore",
+    date: "Apr 2017 - Mar 2018",
+    desc: "I completed my class 12 high school education at Adarsh Bal Vinay Mandir, Indore, where I studied Science with Computer Science.",
+    degree: "MPBSE(XII), Science with Computer",
+  },
+
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6KtCz4kQ5SEhlbeDe2DO8AuOxdyrW1UmrZKzsnwEfZcmcV0QNJsX2I82DZcfC_Oq_cow&usqp=CAU",
+    school: "Adarsh Bal Vinay Mandir, Indore",
+    date: "Apr 2015 - Mar 2016",
+    desc: "I completed my class 10 education at Adarsh Bal Vinay Mandir, Indore, where I studied Science with Computer Application.",
+    degree: "MPBSE(X), Science with Computer",
+  },
+
+
+  
+];
+
+export const projects = [
+//  {
+//    id: 9,
+//    title: "Bank Of Bharat (BOB)",
+//    date: "Jun 2023 - Aug 2023",
+//    description:
+//      "implemented all the functionalities that a typical bank application has in the backend.",
+//    image:
+//      "https://i.ytimg.com/vi/jf6kd4bUNGY/maxresdefault.jpg",
+//    tags: [
+//      "Spring Boot",
+//      "Spring MVC",
+//      "Hibernate",
+//      "Spring Cloud",
+//      "Spring Data",
+//      "CI/CD",
+//      "Apache Camel",
+//      "MongoDb",
+//      "Msql",
+//      "Microservices",
+//    ],
+//    category: "web app",
+//    github: "https://github.com/HimanshuSingour/Bank-Of-Bharat-BOB",
+//  },
+//  {
+//    id: 0,
+//    title: "AlgorithmXlr8",
+//    date: "Apr 2023 - May 2023",
+//    description:
+//      "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+//    image:
+//      "https://i.ibb.co/NZY49cJ/SCR-20240511-bpyw.png",
+//    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+//    category: "web app",
+//    github: "https://github.com/rishavchanda/Podstream",
+//    webapp: "https://podstream.netlify.app/",
+//
+//  },
+//  {
+//    id: 1,
+//    title: "UPI Payment Application",
+//    date: "Oct 2022 - Present",
+//    description:
+//      "UPI payment backend, built on Spring Boot and Microservices, integrates design patterns for streamlined operations and security.",
+//    image:
+//      "https://assets-global.website-files.com/63abfe1dce1c7d5156ec0d56/64b7bc80ca5573c38df66efb_upi_apps_thumbnail.webp",
+//    tags: [
+//      "Spring Framework",
+//      "Micorservice Arcticture",
+//      "Design Patterns",
+//      "Apache Kafka",
+//      "Apache Camel",
+//    ],
+//
+//    github: "https://github.com/HimanshuSingour/UPI-Payment-Application",
+//
+//  },
+  {
+    id: 2,
+    title: "RailWay Application",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Made a railway app like India's own, with all the features you'd expect. Book tickets, check schedules, and travel hassle-free",
+    image:
+      "https://static.toiimg.com/thumb/msid-80042755,width-1280,height-720,resizemode-4/80042755.jpg",
+      tags: [
+        "Spring Framework",
+        "Micorservice Arcticture",
+        "Design Patterns",
+        "Apache Kafka",
+        "Apache Camel",
+      ],
+    github: "https://github.com/HimanshuSingour/India-RailwayWise-Your-Journey-Our-Management",
+
+    
+  },
+//  {
+//    id: 3,
+//    title: "InfyGo Flight Booking System",
+//    date: "Dec 2021 - Apr 2022",
+//    description:
+//      "Built a flight booking app for easy travel planning. Book seats, check flights – all in one place",
+//    image:
+//      "https://i.ytimg.com/vi/dtTkuy_Jpy8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAYoyggQWDAHKy9vbwvcR2yQOdgOQ",
+//      tags: [
+//        "Spring Framework",
+//        "Micorservice Arcticture",
+//        "Design Patterns",
+//        "Apache Kafka",
+//        "Apache Camel",
+//        "Mysql",
+//        "MongosDb"
+//      ],
+//
+//    github: "https://github.com/HimanshuSingour/Flight-Booking-Application-",
+//
+//  },
+  {
+    id: 10,
+    title: "Movie Mate",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Meet Movi Mate: Your go-to for movies. Find, book, and enjoy your favorite films effortlessly.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:1200/1*YikEKg0FVtYuSHSKws7SKg.png",
+      tags: [
+        "Spring Framework",
+        "Micorservice Arcticture",
+        "Design Patterns",
+        "Apache Kafka",
+        "Apache Camel",
+      ],
+
+    github: "https://github.com/HimanshuSingour/MovieMate",
+
+  },
+
+  {
+    id: 5,
+    title: "Hotel Management",
+    date: "Jun 2021",
+    description:
+      " Meet our Hotel Management System: It handles everything from bookings to checkouts. Easily manage reservations, room assignments, and guest services, making hotel operations smooth as silk.",
+    image:
+      "https://uizard.io/static/4051c716723dd5a46d8490f7c5c0ad51/a8e47/16cecc90d113370bb651297d7d86e4b24906af40-1440x835.png",
+      tags: [
+        "Spring Framework",
+        "Micorservice Arcticture",
+        "Design Patterns",
+        "Apache Kafka",
+        "Apache Camel",
+      ],
+    github: "https://github.com/HimanshuSingour/Hotel-Management-Microservice-SpringBoot",
+
+  },
+//  {
+//    id: 6,
+//    title: "Google Pay Application",
+//    date: "Jun 2021",
+//    description:
+//      "A versatile payment application inspired by the original. With features like money transfer, bill splitting, shopping, rewards, and intuitive financial management, our app mirrors the convenience and functionality of Google Pay, ensuring seamless transactions for users",
+//    image:
+//      "https://i.pcmag.com/imagery/reviews/07y0CcC1S2BwbOvOlSKXTtT-9.fit_lim.size_840x473.v1569478436.png",
+//      tags: [
+//        "Apache Camel",
+//        "Spring Framework",
+//        "Micorservice",
+//        "Design Patterns",
+//        "Apache Kafka",
+//
+//      ],
+//    github: "https://github.com/HimanshuSingour/Google-Pay-Payment-Application-",
+//
+//  },
+//  {
+//    id: 7,
+//    title: "E-Commerce Platform",
+//    date: "Jul 2022",
+//    description: "A robust e-commerce platform built with Java full-stack, React.js, and DevOps practices, featuring seamless product browsing, order management, and payment processing. The platform integrates microservices with Apache Kafka for real-time data flow and utilizes CI/CD pipelines for efficient deployment and scaling.",
+//    image: "https://files.oaiusercontent.com/file-MFqovcEbuGD1Q80xTtDmQHey?se=2024-11-07T09%3A50%3A26Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D69ea8669-3b51-49a4-abec-0c13fcaa7602.webp&sig=nHFQJZMVrifUFlnodBFaTcPa57/k4Ax%2BkBmH9wqnBlg%3D",
+//    tags: [
+//      "Java Full-Stack",
+//      "React.js",
+//      "DevOps",
+//      "Microservices",
+//      "Apache Kafka",
+//      "CI/CD"
+//    ],
+//    github: "https://github.com/HimanshuSingour/E-Commerce-Application-Spring-React-"
+//  },
+  {
+    id: 8,
+    title: "Recruitment Platform",
+    date: "Aug 2023",
+    description: "A comprehensive recruitment platform built using Java full-stack, React.js, and DevOps practices. The platform streamlines the hiring process by enabling job listings, applicant tracking, interview scheduling, and real-time notifications, while ensuring scalability and performance with microservices and automated CI/CD pipelines.",
+    image: "https://emploi-digital.fr/wp-content/uploads/2023/04/Capture-decran-2023-04-02-a-00.34.59-1024x427.jpg",
+    tags: [
+      "Java Full-Stack",
+      "React.js",
+      "DevOps",
+      "Microservices",
+      "CI/CD",
+      "Spring Boot"
+    ],
+    github: "https://github.com/HimanshuSingour/Recruitment-Platform-Spring-Microservices-Kafka-Cloud-Etc..-"
+  },
+  {
+    id: 8,
+    title: "Piggy Metrics Microservices",
+    date: "Aug 2023",
+    description: "A cloud-based microservices architecture for tracking and analyzing financial metrics, built using Spring Framework and deployed on AWS. The system leverages multiple microservices to handle user data, transaction processing, and metrics analytics, ensuring scalability and high availability. Integrated with CI/CD pipelines for automated testing and deployment.",
+    image: "https://www.techfunnel.com/wp-content/uploads/2024/09/Cash-Flow-for-Small-Businesses.jpg",
+    tags: [
+      "Spring Framework",
+      "Microservices",
+      "Cloud",
+      "AWS",
+      "CI/CD",
+      "Metrics Analytics"
+    ],
+    github: "https://github.com/HimanshuSingour/Piggy-Metrics-Microservices-Cloud-Spring-Frame-"
+  },
+
+//  {
+//    id: 9,
+//    title: "BookMyShow.com",
+//    date: "Sep 2023",
+//    description: "A full-stack clone of the BookMyShow platform, built with Java Spring Boot for the backend and React.js for the frontend. The application includes features like movie booking, seat selection, user authentication, and payment processing. Deployed with Docker and integrated CI/CD pipelines for seamless deployment and scaling on AWS.",
+//    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKzvYTA4IMLa2OlpVvOU5a_NiM7CsQRFgnAuW7gWClqB_Pk9h_U2eFdofivWHiiOHnFls&usqp=CAU",
+//    tags: [
+//      "Java Spring Fremwork",
+//      "Microservices",
+//      "React.js",
+//      "Full-Stack",
+//      "CI/CD",
+//      "AWS",
+//      "Docker",
+//      "Payment Integration"
+//    ],
+//    github: "https://github.com/HimanshuSingour/BookMyShow-WebApp"
+//  }
+//
+//
+//  
+  
+  
+  
+
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
